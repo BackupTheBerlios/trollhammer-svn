5ba66e1f24ffdadbbb65fd312b4f98bd2213f2bb
sambuc <sambuc@3be4125e-a710-0410-b45b-fd6bf7a62d9a>
2006-04-27 20:29:05 +0000

    Application de la nouvelle arborescence (on prie pour qu'il me fasse tout d'un coup, et d'un seul)

---
+++ src/client/UserManagerClient.java
@@ -0,0 +1,36 @@
+package trollhammer;
+import java.util.Set;
+
+class UserManagerClient {
+
+    private Set<Utilisateur> utilisateurs;
+
+    void détailsUtilisateur(Utilisateur u) {
+        for(Utilisateur util : utilisateurs) {
+            if(u.getLogin().equals(util.getLogin())) {
+                utilisateurs.remove(util);
+            }
+        }
+        utilisateurs.add(u);
+    }
+
+    void listeUtilisateurs(Set<Utilisateur> ul) {
+        utilisateurs = ul;
+        Onglet m = Client.client.getMode();
+        if(m == Onglet.GestionUtilisateurs) {
+            Client.hi.affichageListeUtilisateurs(ul);
+        }
+    }
+
+    /** Cherche un utilisateur par son identifiant et le retourne,
+     * ou null si non trouvé.
+     */
+    Utilisateur getUser(String i) {
+        for(Utilisateur u : utilisateurs) {
+            if(u.getLogin().equals(i)) {
+                return u;
+            }
+        }
+        return null;
+    }
+}

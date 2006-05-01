2dff01c30e6a1524bd831df90ec7e3b09f5dd7a4
dolarcles <dolarcles@3be4125e-a710-0410-b45b-fd6bf7a62d9a>
2006-05-01 14:26:37 +0000

    Retour à la révision 58

---
+++ src/client/Utilisateur.java
@@ -0,0 +1,28 @@
+package trollhammer;
+
+class Utilisateur extends Participant {
+
+    private String mot_de_passe;
+
+    Utilisateur(String login, String nom, String prenom, String mot_de_passe) {
+        super(login, nom, prenom);
+        this.mot_de_passe = mot_de_passe;
+    }
+
+    String getMotDePasse() {
+        return this.mot_de_passe;
+    }
+
+    void setMotDePasse(String mot_de_passe) {
+        this.mot_de_passe = mot_de_passe;
+    }
+
+}
+
+class Moderateur extends Utilisateur {
+
+    Moderateur(String login, String nom, String prenom, String mot_de_passe) {
+        super(login, nom, prenom, mot_de_passe);
+    }
+
+}
---
+++ src/serveur/Objet.java
@@ -0,0 +1,78 @@
+package trollhammer;
+
+class Objet implements java.io.Serializable {
+    
+    private int id;
+    private int nom;
+    private int description;
+    private int prix_de_base;
+    private int prix_de_vente;
+    private StatutObjet statut;
+    private String acheteur;
+    private String vendeur;
+
+    int getId() {
+        return this.id;
+    }
+
+    int getNom() {
+        return this.nom;
+    }
+
+    int getDescription() {
+        return this.description;
+    }
+
+    int getPrixDeBase() {
+        return this.prix_de_base;
+    }
+
+    int getPrixDeVente() {
+        return this.prix_de_vente;
+    }
+
+    StatutObjet getStatut() {
+        return this.statut;
+    }
+
+    String getAcheteur() {
+        return this.acheteur;
+    }
+
+    String getVendeur() {
+        return this.vendeur;
+    }
+
+    void setId() {
+        this.id = id;
+    }
+
+    void setNom() {
+        this.nom = nom;
+    }
+
+    void setDescription() {
+        this.description = description;
+    }
+
+    void setPrixDeBase() {
+        this.prix_de_base = prix_de_base;
+    }
+
+    void setPrixDeVente() {
+        this.prix_de_vente = prix_de_vente;
+    }
+
+    void setStatut() {
+        this.statut = statut;
+    }
+
+    void setAcheteur() {
+        this.acheteur = acheteur;
+    }
+
+    void setVendeur() {
+        this.vendeur = vendeur;
+    }
+
+}

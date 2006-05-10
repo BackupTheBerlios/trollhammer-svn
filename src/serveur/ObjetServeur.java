c882a5656490bb8089b46a43b5016d28da71cc5c
jruffin <jruffin@3be4125e-a710-0410-b45b-fd6bf7a62d9a>
2006-05-10 17:15:43 +0000

    Implémentation des getters/setters dans ObjetServeur.

--- src/serveur/ObjetServeur.java
+++ src/serveur/ObjetServeur.java
@@ -36,4 +36,76 @@ class ObjetServeur {
 	Objet getObjet() {
 		return objet;
 	}
+
+    int getId() {
+        return this.objet.getId();
+    }
+
+    String getNom() {
+        return this.objet.getNom();
+    }
+
+    String getDescription() {
+        return this.objet.getDescription();
+    }
+
+    String getModerateur() {
+        return this.objet.getModerateur();
+    }
+
+    int getPrixDeBase() {
+        return this.objet.getPrixDeBase();
+    }
+
+    int getPrixDeVente() {
+        return this.objet.getPrixDeVente();
+    }
+
+    StatutObjet getStatut() {
+        return this.objet.getStatut();
+    }
+
+    String getAcheteur() {
+        return this.objet.getAcheteur();
+    }
+
+    String getVendeur() {
+        return this.objet.getVendeur();
+    }
+
+    void setId(int id) {
+        this.objet.setId(id);
+    }
+
+    void setNom(String nom) {
+        this.objet.setNom(nom);
+    }
+
+    void setDescription(String description) {
+        this.objet.setDescription(description);
+    }
+
+    void setModerateur(String moderateur) {
+        this.objet.setModerateur(moderateur);
+    }
+
+    void setPrixDeBase(int prix_de_base) {
+        this.objet.setPrixDeBase(prix_de_base);
+    }
+
+    void setPrixDeVente(int prix_de_vente) {
+        this.objet.setPrixDeVente(prix_de_vente);
+    }
+
+    void setStatut(StatutObjet statut) {
+        this.objet.setStatut(statut);
+    }
+
+    void setAcheteur(String acheteur) {
+        this.objet.setAcheteur(acheteur);
+    }
+
+    void setVendeur(String vendeur) {
+        this.objet.setVendeur(vendeur);
+    }
 }

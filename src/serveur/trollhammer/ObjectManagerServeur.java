e13c07f0482a9133bc10d0964c14418229350bb9
sambuc <sambuc@3be4125e-a710-0410-b45b-fd6bf7a62d9a>
2006-05-14 09:05:39 +0000

    CoupdeMASSE() implémenté...
    A part des correction sur les utilisation de Logger .log au niveau d client, toutes les fonction du modèle sont implémentée, a ma connaissance, par contre je constate qu'il manque encores des chose niveau de l'interface de client (GUI)... mais sur ce point je vous laisse avancer...
    
    (je serai lundi à 0800 en INF2, histoire de me faire peur...)

--- src/serveur/trollhammer/ObjectManagerServeur.java
+++ src/serveur/trollhammer/ObjectManagerServeur.java
@@ -112,23 +112,4 @@ class ObjectManagerServeur {
 		objets.add(o);
     }
 
-    /**
-     * "Vend" l'objet, c-à-d affecte son prix de vente, l'acheteur, et change 
-     * son statut.
-     *
-     * @param	i		acheteur
-     * @param	prix	prix
-     * @param	oid		identifiant de l'objet
-     */
-    void sell(String i, int prix, int oid) {
-    	ObjetServeur o = this.getObjet(oid);
-    	
-    	if (o != null) {
-    		o.setPrixDeVente(prix);
-    		o.setAcheteur(i);
-    		o.setStatut(StatutObjet.Vendu);
-    	}
-// là on a un problème si o == null mais que faire ?
-    }
-	
 }

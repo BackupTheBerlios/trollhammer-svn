7aab99fd6ea741cf1c66bb45daa50fe362e60b84
cfrey <cfrey@3be4125e-a710-0410-b45b-fd6bf7a62d9a>
2006-05-15 11:13:35 +0000

    ajout commentaires javadoc a ChatSystem.

--- src/serveur/trollhammer/ChatSystem.java
+++ src/serveur/trollhammer/ChatSystem.java
@@ -5,12 +5,21 @@ package trollhammer;
  * Encore très basique, elle est un sous-système à elle seule car elle pourrait
  * se voir dotée de beaucoup plus de fonctionnalités. Pour l'instant, elle se
  * contente d'ordonner un broadcast des messages de chat qui lui sont donnés.
- * @author squelette : Julien Ruffin, implémentation : Julien Ruffin
+ *
+ * @author	jruffin
  */
 class ChatSystem {
 
+	/**
+	 * Utilise le broadcaster pour envoyer à tous les utilisateurs connectés le
+	 * message provenant du sender.
+	 *
+	 * @param	message	message
+	 * @param	sender	identifiant utilisateur
+	 */
     void envoyerChat(String message, String sender) {
-        Logger.log("ChatSystem", 2, LogType.DBG, "[chat] " + sender + " dit : \"" + message + "\"");
+        Logger.log("ChatSystem", 2, LogType.DBG, "[chat] " + sender
+        	+ " dit : \"" + message + "\"");
         Serveur.broadcaster.chat(message, sender);
     }
 
